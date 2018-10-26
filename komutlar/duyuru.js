e22011b86759b3ad20210e903d163f2485ca48a8
lazusakan123123 <44501653+lazusakan123123@users.noreply.github.com>
2018-10-26 17:55:25 +0300

    Update and rename komutlar/duyuru.js to  komutlar/duyuru.js

--- komutlar/duyuru.js
+++ komutlar/duyuru.js
@@ -1,25 +1,43 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-	let mesaj = args.slice(0).join(' ');
-	if (mesaj.length < 1) return message.reply('Yazmam İçin Birşey Yazmalısın!');
-    message.delete();
-    const embed = new Discord.RichEmbed()
-    .setAuthor('')
-    .setColor(3447003)
-    .setDescription(`${mesaj}`)
-    return message.channel.sendEmbed(embed);
+
+    if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+    .setColor(0x2488E7)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField('Hey Sen Napıyorsun', 'Ben Sunucu Botuyum Lütfen Beni Sunucunda Dene.')
+    return message.author.sendEmbed(ozelmesajuyari); }
+
+  let mesaj = args.slice(0).join(' ');
+if (mesaj.length < 1) return message.channel.send('Birşey Yazmalısınız');
+
+  message.delete();
+
+  console.log(`Duyuru: "${message.author.username}#${message.author.discriminator}" "${mesaj}"`);
+
+      const mesajat = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setDescription('' + mesaj + '')
+
+      client.users.forEach(u => {
+u.sendEmbed(mesajat)
+})
+
+message.channel.send(`:white_check_mark: Mesaj basariyla **` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + `** kullanıcıya gonderildi.`);
+
 };
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: ['duyuryap', 'duyur'],
-  permLevel: 2
+  guildOnly: true,
+  aliases: ['duyurla'],
+  permLevel: 4
 };
 
 exports.help = {
-  name: 'duyuru',
-  description: 'Güzel Bir Duyuru Görünümü Sağlar.',
-  usage: 'duyuru [Duyuruda Yazıcak Şey]'
+  name: 'duyur',
+  description: 'İstediğiniz şeyi bota duyurtur.',
+  usage: 'duyuru [duyurmak istediğiniz şey]'
 };
